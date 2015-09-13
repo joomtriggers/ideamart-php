cf9595264ccb9167bd08a5e93f66e8e76b335a85
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-09-13 12:21:21 +0530

    UPDATE: adding dummy test files
    SUMMARY: added dummy tests files for the Handler
    
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

---
+++ tests/HandlerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class: HandlerTest
+ *
+ * PHP Version 5.6
+ *
+ * @category Testing
+ * @package  Ideamart
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://github.com/joomtriggers/ideamart-php/
+ *
+ * @see TestCase
+ */
+
+namespace Joomtriggers\Ideamart\Tests;
+
+/**
+ * Class: HandlerTest
+ *
+ * @category Testing
+ * @package  Ideamart
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://github.com/joomtriggers/ideamart-php/
+ *
+ * @see TestCase
+ */
+class HandlerTest extends TestCase
+{
+
+    public function testSample(){}
+
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Class: TestCase
+ *
+ * PHP Version 5.6
+ *
+ * @category Testing
+ * @package  Ideamart
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://github.com/joomtriggers/ideamart-php/
+ *
+ * @see PHPUnit_Framework_TestCase
+ */
+namespace Joomtriggers\Ideamart\Tests;
+
+
+use PHPUnit_Framework_TestCase;
+/**
+ * Class: TestCase
+ *
+ * @category Testing
+ * @package  Ideamart
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://github.com/joomtriggers/ideamart-php/
+ *
+ * @see PHPUnit_Framework_TestCase
+ */
+class TestCase extends PHPUnit_Framework_TestCase
+{
+
+}
+
