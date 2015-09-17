7e05379b4e78f78f40dfc5585a10c9a873a6588b
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2015-09-17 08:10:42 +0530

    UPDATE: adding core trait function
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- src/Core.php
+++ src/Core.php
@@ -5,9 +5,26 @@ namespace Joomtriggers\Ideamart;
 
 trait Core {
 
+    /**
+     * @param $jsonResponse
+     * @param $url
+     */
+    public function sendRequest($jsonResponse, $url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array ('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonResponse);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
 
-    public function send(){
+        $this->log->info("SENDING REQUEST :" . $jsonResponse);
+        $this->log->info("SENDING REQUEST TO URL:" . $url);
+        $this->log->info("RECEIVED RESPONSE :" . $response);
 
+        return $response;
     }
 }
 
