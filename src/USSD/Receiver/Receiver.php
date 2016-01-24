536998b5cef3607a4e4eff540e68fed5495a0b0c
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2016-01-24 09:06:36 +0530

    UPDATE
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- src/USSD/Receiver/Receiver.php
+++ src/USSD/Receiver/Receiver.php
@@ -1,26 +1,62 @@
 <?php
+/**
+ * Class: Receiver
+ *
+ * PHP Version: 7.0.0
+ *
+ * @category Category
+ * @package  Package
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://link/
+ */
 
 namespace Ideamart\USSD\Ideamart\Receiver;
 
-class Receiver {
+/**
+ * Class: Receiver
+ *
+ * @category Category
+ * @package  Package
+ * @author   Gnanakeethan Balasubramaniam <gnana@keethan.me>
+ * @license  MIT http://opensource.org/licenses/MIT/
+ * @link     http://link/
+ */
+class Receiver
+{
 
-	protected $request = [];
+    protected $request = [];
 
-	protected $validator;
+    protected $validator;
 
-	protected $formatter;
+    protected $formatter;
 
-	public function __construct(RequestFormatter $formatter, RequestValidator $validator) {
-		$this->validator = $validator;
-		$this->formatter = $formatter;
+    /**
+     * Constructor
+     *
+     * @param RequestFormatter $formatter Formatter
+     * @param RequestValidator $validator Validator
+     */
+    public function __construct(RequestFormatter $formatter, RequestValidator $validator)
+    {
+        $this->validator = $validator;
+        $this->formatter = $formatter;
 
-	}
+    }
 
-	public function receive(array $request) {
-		$request = $this->validator->validate($request);
-		$this->request = $this->formatter->format($request);
+    /**
+     * Receiver
+     *
+     * @param array $request Request Array
+     *
+     * @return Object
+     */
+    public function receive(array $request)
+    {
+        $request = $this->validator->validate($request);
+        $this->request = $this->formatter->format($request);
 
-		return $request;
-	}
+        return $this->request;
+    }
 
 }
--- src/USSD/Receiver/RequestValidator.php
+++ src/USSD/Receiver/RequestValidator.php
@@ -8,28 +8,10 @@ class RequestValidator {
 
 	protected $rules;
 
-	public function __construct(Application $app) {
-		$this->app = $app;
-		$this->rules = [
-			'message' => 'required|min:1|max:512',
-			'requestId' => 'required|numeric',
-			'encoding' => 'required|max:22',
-			'sourceAddress' => 'required|max:73',
-			'version' => 'required|size:3',
-		];
+	public function __construct() {
 	}
 
-	public function validate(array $request) {
-		$validator = $this->app['validator']->make(
-			$request,
-			$this->rules
-		);
-
-		if ($validator->fails()) {
-			\Log::info($validator->failed());
-			\Log::info($request);
-		} else {
-			return $request;
-		}
+	public function validate(array $request,array $rules) {
+        return $request;
 	}
 }
