ca35ae1f1234607d58f1d9b03dbfb142baa34ca9
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2016-01-24 11:23:26 +0530

    UPDATE
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- src/USSD/Maker/MessageTranslator.php
+++ src/USSD/Maker/MessageTranslator.php
@@ -2,51 +2,22 @@
 
 namespace Ideamart\USSD\Ideamart\Maker;
 
-use Ideamart\USSD\Ideamart\Sender\Broker\ServiceBroker;
-use Illuminate\Contracts\Foundation\Application;
-use Inqurtime\Source\Applications\ApplicationRepository;
-use Inqurtime\System\ConfigLoader\Contracts\ConfigLoader;
-use Inqurtime\System\Session\Contracts\SessionHandler;
 
-/**
- * Class MessageTranslator
- * @package Inqurtime\System\USSD\Ideamart\Maker
- */
 class MessageTranslator {
-
-    /**
-     * @var \Inqurtime\System\USSD\Ideamart\Session\Contracts\SessionHandler
-     */
     protected $session;
-
-    /**
-     * @var \Illuminate\Contracts\Config\Config
-     */
-    protected $config;
-
     protected $configLoader;
-
     protected $serviceBroker;
 
-    /**
-     * @param \Inqurtime\System\USSD\Ideamart\Session\Contracts\SessionHandler $sessionHandler
-     */
     public function __construct(SessionHandler $sessionHandler,
         Application $application,
         ConfigLoader $configLoader,
         ServiceBroker $serviceBroker
     ) {
         $this->session = $sessionHandler;
-        $this->config = $application->make('config');
         $this->configLoader = $configLoader;
         $this->serviceBroker = $serviceBroker;
     }
 
-    /**
-     * @param array                                                $request
-     * @param \Inqurtime\System\USSD\Ideamart\Maker\Parser         $parser
-     * @param \Inqurtime\Source\Applications\ApplicationRepository $applicationRepository
-     */
     public function translate(array $request, Parser $parser, ApplicationRepository $applicationRepository) {
         $message = $request['message'];
         $this->session->setParameters($request);
@@ -58,14 +29,6 @@ class MessageTranslator {
         $this->messageIterator($menu['options'], $message, $parser);
     }
 
-    /**
-     * @param array                                        $menu
-     * @param array                                        $message
-     *
-     * @param \Inqurtime\System\USSD\Ideamart\Maker\Parser $parser
-     *
-     * @return string
-     */
     private function messageIterator(array $menu, array $message, Parser $parser) {
 
         if (empty($message)) {
@@ -110,11 +73,6 @@ class MessageTranslator {
         return $this->session;
     }
 
-    /**
-     * @param string $message
-     *
-     * @return array
-     */
     private function messageConverter($message = "") {
 
         $code = $this->config->get('system.' . $this->session->getAppId() . '.ussd.code');
