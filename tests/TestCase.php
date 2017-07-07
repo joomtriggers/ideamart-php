0f922c86d75353888d566b1742c4fdb072137847
Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>
2017-07-07 22:16:03 +0530

    iU
    Signed-off-by: Gnanakeethan Balasubramaniam <gnanakeethan@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,10 @@
 namespace Joomtriggers\Ideamart\Tests;
 use \PHPUnit_Framework_TestCase;
 
-
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+	class_exists('\PHPUnit_Framework_TestCase')) {
+	class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
 /**
  * Class: TestCase
  *
